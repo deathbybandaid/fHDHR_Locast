8297d97df5471bd59083bb1209cc8831383e6afe
deathbybandaid <github@deathbybandaid.net>
2021-01-07 14:49:17 -0500

    test

--- fHDHR_web/pages/diagnostics_html.py
+++ fHDHR_web/pages/diagnostics_html.py
@@ -23,20 +23,21 @@ class Diagnostics_HTML():
             if route_group not in ["pages", "brython", "files"]:
                 button_dict[route_group] = []
                 for route_item in list(session["route_list"][route_group].keys()):
-                    button_link = session["route_list"][route_group][route_item]["endpoints"][0]
-                    parameter_index = 0
-                    for parameter in list(session["route_list"][route_group][route_item]["endpoint_default_parameters"].keys()):
-                        parameter_val = session["route_list"][route_group][route_item]["endpoint_default_parameters"][parameter]
-                        if not parameter_index:
-                            button_link += "?"
-                        else:
-                            button_link += "&"
-                        button_link += "%s=%s" % (parameter, parameter_val)
-                    button_link = button_link.replace("<devicekey>", self.fhdhr.config.dict["main"]["uuid"])
-                    button_link = button_link.replace("<base_url>", base_url)
-                    button_dict[route_group].append({
-                                        "label": session["route_list"][route_group][route_item]["pretty_name"],
-                                        "link": button_link,
-                                        })
+                    if not session["route_list"][route_group][route_item]["name"].startswith("page_"):
+                        button_link = session["route_list"][route_group][route_item]["endpoints"][0]
+                        parameter_index = 0
+                        for parameter in list(session["route_list"][route_group][route_item]["endpoint_default_parameters"].keys()):
+                            parameter_val = session["route_list"][route_group][route_item]["endpoint_default_parameters"][parameter]
+                            if not parameter_index:
+                                button_link += "?"
+                            else:
+                                button_link += "&"
+                            button_link += "%s=%s" % (parameter, parameter_val)
+                        button_link = button_link.replace("<devicekey>", self.fhdhr.config.dict["main"]["uuid"])
+                        button_link = button_link.replace("<base_url>", base_url)
+                        button_dict[route_group].append({
+                                            "label": session["route_list"][route_group][route_item]["pretty_name"],
+                                            "link": button_link,
+                                            })
 
         return render_template('diagnostics.html', session=session, request=request, fhdhr=self.fhdhr, button_dict=button_dict, list=list)
