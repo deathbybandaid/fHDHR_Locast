0fbeb0929c0f170491739cf2d2b8f3c1a93f6e8a
deathbybandaid <github@deathbybandaid.net>
2021-01-13 10:59:02 -0500

    test

--- fHDHR_web/api/channels.py
+++ fHDHR_web/api/channels.py
@@ -148,6 +148,7 @@ class Channels():
             return "Invalid Method"
 
         if redirect_url:
+            return redirect(redirect_url + "?retmessage=" + urllib.parse.quote("%s Success" % method))
             return redirect("%s?retmessage=%s" % (redirect_url, urllib.parse.quote("%s Success" % method)))
         else:
             if method == "scan":
