2c3c8d83575c8d069b6ccaa56f65d90d288e1c0d
deathbybandaid <github@deathbybandaid.net>
2021-01-21 15:11:44 -0500

    test

--- alternative_epg/__init__.py
+++ alternative_epg/__init__.py
@@ -5,7 +5,7 @@ for entry in os.scandir(alt_epg_top_dir):
     if entry.is_dir() and not entry.is_file() and entry.name[0] != '_':
         alt_epg_name = entry.name
         alt_epg_dir_contents = [x for x in os.scandir("%s/%s" % (alt_epg_top_dir, alt_epg_name))]
-        if "%s_conf.json" % alt_epg_name in alt_epg_dir_contents and "__init__.py" in alt_epg_dir_contents:
-            imp_string = "from .%s import *" % alt_epg_name
-            print(entry)
-            exec(imp_string)
+        # if "%s_conf.json" % alt_epg_name in alt_epg_dir_contents and "__init__.py" in alt_epg_dir_contents:
+        imp_string = "from .%s import *" % alt_epg_name
+        print(entry)
+        exec(imp_string)
