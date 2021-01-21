f9371709ea723154f28552bc9ac2f79585942d8a
deathbybandaid <github@deathbybandaid.net>
2021-01-21 14:59:08 -0500

    test

--- alternative_epg/__init__.py
+++ alternative_epg/__init__.py
@@ -1,3 +1,9 @@
-# pylama:ignore=W0401,W0611
-from .zap2it import *
-from .tvtv import *
+import os
+
+alt_epg_top_dir = os.path.dirname(__file__)
+for entry in os.scandir(alt_epg_top_dir):
+    if entry.is_dir():
+        alt_epg_name = entry.name[:-3]
+        alt_epg_dir_contents = [x for x in os.scandir("%s/%s" % (alt_epg_top_dir, alt_epg_name))]
+        if "%s_conf.json" % alt_epg_name in alt_epg_dir_contents and "__init__.py" in alt_epg_dir_contents:
+            exec("from .%s import *" % alt_epg_name)
