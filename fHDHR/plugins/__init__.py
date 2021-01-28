41091f56b9ae2b92afd8ac30287b8f534d13ad89
deathbybandaid <github@deathbybandaid.net>
2021-01-28 12:01:52 -0500

    test

--- fHDHR/plugins/__init__.py
+++ fHDHR/plugins/__init__.py
@@ -246,8 +246,3 @@ class PluginsHandler():
         for plugin_name, plugin_path, plugin_conf, plugin_manifest in self.found_plugins:
             plugin_item = Plugin(self.config, self.logger, self.db, plugin_name, plugin_path, plugin_conf, plugin_manifest)
             self.plugins[plugin_item.plugin_dict_name] = plugin_item
-        for plugin_name in list(self.plugins.keys()):
-            if self.plugins[plugin_name].manifest["tagged_mod"]:
-                if self.plugins[plugin_name].manifest["tagged_mod"] not in [self.plugins[x].name.lower() for x in list(self.plugins.keys())]:
-                    self.plugins[plugin_name].manifest["tagged_mod"] = None
-                    self.plugins[plugin_name].manifest["tagged_mod_type"] = None
