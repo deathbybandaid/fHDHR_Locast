3b2399efa22a916457f8b9de787378d412e2e543
deathbybandaid <github@deathbybandaid.net>
2021-01-28 14:31:32 -0500

    test

--- fHDHR_web/pages/tuners_html.py
+++ fHDHR_web/pages/tuners_html.py
@@ -31,7 +31,7 @@ class Tuners_HTML():
 
                 tuner_dict = {
                               "number": str(tuner),
-                              "status": str(tuner_status[tuner]["status"]),
+                              "status": str(tuner_status[origin][tuner]["status"]),
                               "origin": "N/A",
                               "channel_number": "N/A",
                               "method": "N/A",
@@ -40,13 +40,13 @@ class Tuners_HTML():
                               }
 
                 if tuner_status[tuner]["status"] in ["Active", "Acquired", "Scanning"]:
-                    tuner_dict["origin"] = tuner_status[tuner]["origin"]
-                    tuner_dict["channel_number"] = tuner_status[tuner]["channel"] or "N/A"
-                    tuner_dict["running_time"] = str(tuner_status[tuner]["running_time"])
+                    tuner_dict["origin"] = tuner_status[origin][tuner]["origin"]
+                    tuner_dict["channel_number"] = tuner_status[origin][tuner]["channel"] or "N/A"
+                    tuner_dict["running_time"] = str(tuner_status[origin][tuner]["running_time"])
 
                 if tuner_status[tuner]["status"] in "Active":
-                    tuner_dict["method"] = tuner_status[tuner]["method"]
-                    tuner_dict["downloaded"] = humanized_filesize(tuner_status[tuner]["downloaded"])
+                    tuner_dict["method"] = tuner_status[tuner][origin]["method"]
+                    tuner_dict["downloaded"] = humanized_filesize(tuner_status[origin][tuner]["downloaded"])
 
             tuner_status_dict[origin]["status_list"].append(tuner_dict)
 
